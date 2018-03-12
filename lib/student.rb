075e922ea306937a49f820085c3f856c9b2d9f0d
Erin Walker <erinb0317@hotmail.com>
2018-03-12 11:48:11 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,5 +29,18 @@ class Student
       student
     end
 
+    def save 
+      if self.id 
+        self.update 
+      else 
+        sql = <<-SQL 
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+        SQL 
+        DB[:conn].execute(sql, self.name, self.grade)
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+        end
+        end 
+
 
 end
