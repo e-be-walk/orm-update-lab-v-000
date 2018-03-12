bb25bfad257899a97ca93a3c1ca12787af5a2bae
Erin Walker <erinb0317@hotmail.com>
2018-03-12 12:00:23 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,6 +27,19 @@ class Student
       DB[:conn].execute("DROP TABLE students;")
     end
 
+    def save
+      if self.id
+        self.update
+      else
+        sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+        SQL
+        DB[:conn].execute(sql, self.name, self.grade)
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+      end
+    end
+
     def self.new_from_db
       new_student = self.new
       new_student.id = row[0]
@@ -51,19 +64,6 @@ class Student
       student = Student.new(name, grade)
       student.save
       student
-    end
-
-    def save
-      if self.id
-        self.update
-      else
-        sql = <<-SQL
-        INSERT INTO students (name, grade)
-        VALUES (?, ?)
-        SQL
-        DB[:conn].execute(sql, self.name, self.grade)
-        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-      end
     end
 
     def update
