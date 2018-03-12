e19e8d854226ef4868fec66841b34827e5bb4c4d
Erin Walker <erinb0317@hotmail.com>
2018-03-12 12:07:40 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,16 +53,6 @@ class Student
       self.new(id, name, grade)
     end
 
-    def update
-      sql = <<-SQL
-      UPDATE students
-      SET name = ?, grade = ?
-      WHERE id = ?
-      SQL
-
-      DB[:conn].execute(sql, self.name, self.grade, self.id)
-    end
-
     def self.find_by_name(name)
       sql = <<-SQL
         SELECT *
@@ -73,6 +63,16 @@ class Student
 
       DB[:conn].execute(sql, name).map do |row| self.new_from_db(row)
       end.first
+    end
+
+    def update
+      sql = <<-SQL
+      UPDATE students
+      SET name = ?, grade = ?
+      WHERE id = ?
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.grade, self.id)
     end
 
 end
